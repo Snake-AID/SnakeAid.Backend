36d4bf69b1bfdb6b93de6b24ee3d023d79b414a5
the-khiem7 <khiemnguyen120216@gmail.com>
2026-02-23 21:45:00 +0700

    feat(core): introduce IHasReportMedia interface for NoSQL relations
    Add interface to standardized domains that have Polymorphic attachments like SnakeCatchingRequest and SnakebiteIncident.

--- SnakeAid.Core/Domains/SnakeCatchingRequest.cs
+++ SnakeAid.Core/Domains/SnakeCatchingRequest.cs
@@ -8,7 +8,7 @@ using NetTopologySuite.Geometries;
 
 namespace SnakeAid.Core.Domains
 {
-    public class SnakeCatchingRequest : BaseEntity
+    public class SnakeCatchingRequest : BaseEntity, IHasReportMedia
     {
         [Key]
         public Guid Id { get; set; }
