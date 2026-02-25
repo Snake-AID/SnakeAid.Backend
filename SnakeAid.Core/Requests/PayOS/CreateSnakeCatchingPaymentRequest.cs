8be28961c63932e46e8e649a95c76643797701b8
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2026-02-25 18:59:21 +0700

    feat: Enhance payment request handling by incorporating TransactionType in CreateSnakeCatchingPaymentRequest and updating related logging and validation in PayOsPaymentService

--- SnakeAid.Core/Requests/PayOS/CreateSnakeCatchingPaymentRequest.cs
+++ SnakeAid.Core/Requests/PayOS/CreateSnakeCatchingPaymentRequest.cs
@@ -1,3 +1,5 @@
+using SnakeAid.Core.Domains;
+
 namespace SnakeAid.Core.Requests.PayOs;
 
 public class CreateSnakeCatchingPaymentRequest
@@ -5,4 +7,5 @@ public class CreateSnakeCatchingPaymentRequest
     public Guid SnakeCatchingRequestId { get; set; }
     public decimal Amount { get; set; }
     public string? Description { get; set; }
+    public TransactionType TransactionType { get; set; } = TransactionType.CatchingPayment;
 }
