a8d2d2738568d271da1ce8484c5d86c8dd833d3d
the-khiem7 <khiemnguyen120216@gmail.com>
2026-03-06 07:12:57 +0700

    Add problem description to bookings

---
+++ SnakeAid.Repository/Migrations/20260306035727_AddProblemDescriptionToConsultationBooking.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnakeAid.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProblemDescriptionToConsultationBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProblemDescription",
+                schema: "SnakeAid",
+                table: "ConsultationBookings",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProblemDescription",
+                schema: "SnakeAid",
+                table: "ConsultationBookings");
+        }
+    }
+}
