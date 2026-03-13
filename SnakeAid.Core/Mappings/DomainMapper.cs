63a934d031812023108fc9cf97e90b5998f7b456
DuongNManh <manhduonglhp4@gmail.com>
2026-03-13 23:22:39 +0700

    feat: Refactor symptom and rescue request session mappings; remove unused classes and add new shift request models

--- SnakeAid.Core/Mappings/DomainMapper.cs
+++ SnakeAid.Core/Mappings/DomainMapper.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Mapster;
 using SnakeAid.Core.Domains;
 using SnakeAid.Core.Responses.SnakebiteIncident;
-using SnakeAid.Core.Responses.RescueRequestSession;
 using SnakeAid.Core.Responses.FirstAidGuideline;
 using SnakeAid.Core.Responses.SymptomConfig;
 using SnakeAid.Core.Responses.Media;
@@ -20,18 +19,11 @@ namespace SnakeAid.Core.Mappings
             config.NewConfig<NetTopologySuite.Geometries.Point, NetTopologySuite.Geometries.Point>()
                 .MapWith(src => src);
 
-            // RescueRequestSession mappings
-            config.NewConfig<RescueRequestSession, CreateRescueRequestSessionResponse>();
-
-            // SnakebiteIncident mappings
-            config.NewConfig<SnakebiteIncident, CreateIncidentResponse>();
-            config.NewConfig<SnakebiteIncident, UpdateSymptomReportResponse>();
-
             // FirstAidGuideline mappings
             config.NewConfig<FirstAidGuideline, FirstAidGuidelineResponse>();
 
             // SymptomConfig mappings
-            config.NewConfig<Core.Domains.SymptomConfig, SymptomConfigResponse>()
+            config.NewConfig<SymptomConfig, SymptomConfigResponse>()
                 .Map(dest => dest.CategoryDisplay, src => src.Category.ToString())
                 .Map(dest => dest.VenomType, src => src.VenomType != null
                     ? new VenomTypeInfo
@@ -43,9 +35,6 @@ namespace SnakeAid.Core.Mappings
 
             // VenomType to VenomTypeInfo mapping
             config.NewConfig<VenomType, VenomTypeInfo>();
-
-            // Media mappings
-            config.NewConfig<ReportMedia, ReportMediaResponse>();
         }
     }
 }
---
+++ SnakeAid.Core/Responses/RescuerProfile/OnDutyRescuerSnapshotResponse.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace SnakeAid.Core.Responses.RescuerProfile
+{
+    public class OnDutyRescuerSnapshotResponse
+    {
+        public Guid? IncidentId { get; set; }
+        public DateOnly Date { get; set; }
+        public DateTime SnapshotAt { get; set; }
+        public List<OnDutyRescuerItemResponse> Rescuers { get; set; } = new();
+    }
+
+    public class OnDutyRescuerItemResponse
+    {
+        public Guid RescuerId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsOnDutyNow { get; set; }
+        public string AssignmentStatus { get; set; } = string.Empty;
+        public Guid ShiftAssignmentId { get; set; }
+        public Guid ShiftId { get; set; }
+        public string ShiftName { get; set; } = string.Empty;
+        public TimeSpan ShiftStartTime { get; set; }
+        public TimeSpan ShiftEndTime { get; set; }
+        public DateOnly ShiftDate { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public DateTime? LastLocationUpdate { get; set; }
+        public double? DistanceKm { get; set; }
+    }
+}
