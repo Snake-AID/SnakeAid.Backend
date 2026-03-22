e8d7d41a2201c602396c08dc948eed9e22dff6a9
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2026-03-22 17:27:46 +0700

    feat(treatment-facility): add IsActive property to TreatmentFacilityResponse

--- SnakeAid.Core/Responses/TreatmentFacility/TreatmentFacilityResponse.cs
+++ SnakeAid.Core/Responses/TreatmentFacility/TreatmentFacilityResponse.cs
@@ -14,6 +14,7 @@ namespace SnakeAid.Core.Responses.TreatmentFacility
         public double DistanceKm { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public bool IsActive { get; set; }
         public List<int> AntivenomIds { get; set; } = new();
 
     }
