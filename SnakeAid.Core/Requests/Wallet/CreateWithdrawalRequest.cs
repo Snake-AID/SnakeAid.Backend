05c9ae3bb243c311b4aeb38f929a5ae8682fe2f8
the-khiem7 <khiemnguyen120216@gmail.com>
2026-04-03 02:24:10 +0700

    Adds admin withdrawal request DTOs

--- SnakeAid.Core/Requests/Wallet/CreateWithdrawalRequest.cs
+++ SnakeAid.Core/Requests/Wallet/CreateWithdrawalRequest.cs
@@ -5,7 +5,7 @@ namespace SnakeAid.Core.Requests.Wallet
     public class CreateWithdrawalRequest
     {
         [Required]
-        [Range(10000, 50000000)] // Min 10k VND, max 50M VND
+        [Range(50000, 5000000)] // Min 50k VND, max 5M VND
         public decimal Amount { get; set; }
 
         [Required]
@@ -16,6 +16,10 @@ namespace SnakeAid.Core.Requests.Wallet
         [Required]
         [MaxLength(100)]
         public string BankName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string AccountHolderName { get; set; }
 
         [Required]
         [MaxLength(6)]
