3bb81f57a14a5aea38dcfb49279da24683912429
the-khiem7 <khiemnguyen120216@gmail.com>
2026-04-09 16:26:44 +0700

    feat(db): add migration to make Transaction.UserId nullable
    update database schema to make Transaction.UserId nullable
    adjust the model snapshot and FK constraint to reflect nullable UserId
    while preserving OnDelete Restrict behavior

--- SnakeAid.Repository/Migrations/20260409092335_MakeTransactionUserIdNullable.Designer.cs
+++ SnakeAid.Repository/Migrations/20260409092335_MakeTransactionUserIdNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -13,9 +14,11 @@ using SnakeAid.Repository.Data;
 namespace SnakeAid.Repository.Migrations
 {
     [DbContext(typeof(SnakeAidDbContext))]
-    partial class SnakeAidDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409092335_MakeTransactionUserIdNullable")]
+    partial class MakeTransactionUserIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SnakeAid.Repository/Migrations/20260409092335_MakeTransactionUserIdNullable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnakeAid.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeTransactionUserIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                schema: "SnakeAid",
+                table: "Transactions",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                schema: "SnakeAid",
+                table: "Transactions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
